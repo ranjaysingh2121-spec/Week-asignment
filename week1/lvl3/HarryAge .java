49e7ee05e431aa9cc9873188930f23cbb1b9b2a1
ranjaysingh2121-spec <ranjaysingh2121@gmail.com>
2026-03-13 00:36:08 +0530

    wk1_2

---
+++ week1/lvl3/HarryAge .java
@@ -0,0 +1,14 @@
+package week1.lvl3;
+
+public class HarryAge {
+
+    public static void main(String[] args) {
+
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
---
+++ week1/lvl3/KmToMiles.java
@@ -0,0 +1,12 @@
+package week1.lvl3;
+
+public class KmToMiles {
+
+    public static void main(String[] args) {
+
+        double km = 10.8;
+        double miles = km * 1.6;
+
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
